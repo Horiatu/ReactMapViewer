e7f6021f1c26ee78c96db3a204945e599d6dac7c
Horia Tudosie <htudosie@esri.ca>
2018-02-15 12:21:59 -0500

    RMap closure holds this

--- js/Layout.js
+++ js/Layout.js
@@ -1,24 +1,19 @@
 class Layout extends React.Component {
 	componentDidMount() {
-	    if(responsiveVoice.voiceSupport()) {
+	    if(responsiveVoice && responsiveVoice.voiceSupport()) {
 	        responsiveVoice.speak('React Map Viewer');
 	    }
 	}
     render() {
     	return (
     		<div>
-    		
+
 			<NavBar/>			
-    		<RMap/>
+    		<RMap itemId="326320c9eab3489d8d17bc389ce1e023"/>
     		<Help/>
 
     		</div>
     	);
     }
 }
-
-ReactDOM.render(
-    <Layout/>,
-    document.body
-);
 
--- js/RMap.js
+++ js/RMap.js
@@ -1,117 +1,129 @@
 class RMap extends React.Component {
     render() {
-    	return (
-			<div className="calcite-map calcite-map-absolute">
-			    <div id="mapViewDiv"></div>
-			</div>
-    	);
+      	return (
+      			<div className="calcite-map calcite-map-absolute">
+      			    <div id="mapViewDiv"></div>
+      			</div>
+      	);
     }
-	componentDidMount() {
-require([
-  "dojo/dom-class", 
-  // ArcGIS
-  "esri/WebMap",
-  "esri/views/MapView",
+  	constructor(props) {
+        super(props);
+        this.state = {
+            map: null,
+            mapView: null,
+        };
+        const self = this;
 
-  // Widgets
-  "esri/widgets/BasemapGallery",
-  "esri/widgets/Search",
-  "esri/widgets/Legend",
-  "esri/widgets/LayerList",
-  "esri/widgets/Print",
-  "esri/widgets/BasemapToggle",
-  "esri/widgets/ScaleBar",
+        require([
+            "dojo/dom-class", 
+            // ArcGIS
+            "esri/WebMap",
+            "esri/views/MapView",
 
-  // Bootstrap
-  "bootstrap/Collapse",
-  "bootstrap/Dropdown",
+            // Widgets
+            "esri/widgets/BasemapGallery",
+            "esri/widgets/Search",
+            "esri/widgets/Legend",
+            "esri/widgets/LayerList",
+            "esri/widgets/Print",
+            "esri/widgets/BasemapToggle",
+            "esri/widgets/ScaleBar",
 
-  // Calcite Maps
-  "calcite-maps/calcitemaps-v0.5",
-  // Calcite Maps ArcGIS Support
-  "calcite-maps/calcitemaps-arcgis-support-v0.5",
+            // Bootstrap
+            "bootstrap/Collapse",
+            "bootstrap/Dropdown",
 
-  "dojo/domReady!"
-], function(
-  domClass, 
-  WebMap, MapView, Basemaps, Search, Legend, LayerList, 
-  Print, BasemapToggle, ScaleBar, Collapse, Dropdown, 
-  CalciteMaps, CalciteMapArcGISSupport) {
+            // Calcite Maps
+            "calcite-maps/calcitemaps-v0.5",
+            // Calcite Maps ArcGIS Support
+            "calcite-maps/calcitemaps-arcgis-support-v0.5",
+            "dojo/domReady!"
+        ], 
+        function(
+            domClass, 
+            WebMap, MapView, Basemaps, Search, Legend, LayerList, 
+            Print, BasemapToggle, ScaleBar, Collapse, Dropdown, 
+            CalciteMaps, CalciteMapArcGISSupport) {
 
-  /******************************************************************
-   *
-   * Create the map, view and widgets
-   * 
-   ******************************************************************/
+            /******************************************************************
+             *
+             * Create the map, view and widgets
+             * 
+             ******************************************************************/
+            console.log('THIS', self);
 
-  // Map
-  var map = new WebMap({
-    portalItem: {
-      id: "326320c9eab3489d8d17bc389ce1e023"
-    }
-  });
-  
-  // View
-  var mapView = new MapView({
-    container: "mapViewDiv",
-    map: map,
-    padding: {
-      top: 50,
-      bottom: 0
-    }
-  });
+            // Map
+            const map = new WebMap({
+                portalItem: {
+                    id: self.props.itemId
+                }
+            });
+            
+            // View
+            const mapView = new MapView({
+                container: "mapViewDiv",
+                map: map,
+                padding: {
+                    top: 50,
+                    bottom: 0
+                }
+            });
 
-  // Popup and panel sync
-  mapView.then(function(){
-    CalciteMapArcGISSupport.setPopupPanelSync(mapView);
-    domClass.remove(document.body, "app-loading");
-  });
+            self.setState({
+                map: map,
+                mapView: mapView,
+            });
 
-  // Search - add to navbar
-  var searchWidget = new Search({
-    container: "searchWidgetDiv",
-    view: mapView
-  });
-  CalciteMapArcGISSupport.setSearchExpandEvents(searchWidget);
+            // Popup and panel sync
+            mapView.then(function(){
+                CalciteMapArcGISSupport.setPopupPanelSync(mapView);
+                domClass.remove(document.body, "app-loading");
+            });
 
-  // Basemaps
-  var basemaps = new Basemaps({
-    container: "basemapGalleryDiv",
-    view: mapView
-  })
-  
-  // Legend
-  var legendWidget = new Legend({
-    container: "legendDiv",
-    view: mapView
-  });
+            // Search - add to navbar
+            var searchWidget = new Search({
+                container: "searchWidgetDiv",
+                view: mapView
+            });
+            CalciteMapArcGISSupport.setSearchExpandEvents(searchWidget);
 
-  // LayerList
-  var layerWidget = new LayerList({
-    container: "layersDiv",
-    view: mapView
-  });
+            // Basemaps
+            var basemaps = new Basemaps({
+                container: "basemapGalleryDiv",
+                view: mapView
+            })
+            
+            // Legend
+            var legendWidget = new Legend({
+                container: "legendDiv",
+                view: mapView
+            });
 
-  // Print
-  var printWidget = new Print({
-    container: "printDiv",
-    view: mapView,
-    printServiceUrl: "https://utility.arcgisonline.com/arcgis/rest/services/Utilities/PrintingTools/GPServer/Export%20Web%20Map%20Task"
-  });
+            // LayerList
+            var layerWidget = new LayerList({
+                container: "layersDiv",
+                view: mapView
+            });
 
-  // BasemapToggle
-  var basemapToggle = new BasemapToggle({
-    view: mapView,
-    secondBasemap: "satellite"
-  });
-  mapView.ui.add(basemapToggle, "bottom-right");          
-  
-  // Scalebar
-  var scaleBar = new ScaleBar({
-    view: mapView
-  });
-  mapView.ui.add(scaleBar, "bottom-left");
+            // Print
+            var printWidget = new Print({
+                container: "printDiv",
+                view: mapView,
+                printServiceUrl: "https://utility.arcgisonline.com/arcgis/rest/services/Utilities/PrintingTools/GPServer/Export%20Web%20Map%20Task"
+            });
 
-});
-	}
+            // BasemapToggle
+            var basemapToggle = new BasemapToggle({
+                view: mapView,
+                secondBasemap: "satellite"
+            });
+            mapView.ui.add(basemapToggle, "bottom-right");          
+            
+            // Scalebar
+            var scaleBar = new ScaleBar({
+                view: mapView
+            });
+            mapView.ui.add(scaleBar, "bottom-left");
+        });
+	  }
 }
