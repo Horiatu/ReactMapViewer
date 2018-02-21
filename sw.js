92e13b6d0f0092097389975221b004fb1f535a49
Horia Tudosie <htudosie@esri.ca>
2018-02-21 12:18:07 -0500

    cleanuo error reasons

--- sw.js
+++ sw.js
@@ -9,12 +9,12 @@ self.addEventListener('install', function(event) {
         caches.open(CACHE_NAME).then(
             function(cache) {
                 caches.delete(CACHE_NAME).then(function() {
-                    console.log('Opened cache: ', CACHE_NAME, cache.keys());
+                    console.log('Opened cache (install)', CACHE_NAME, cache.keys());
                 });
                 return cache.addAll(urlsToCache);
             },
-            function(error) {
-                console.log('Error cache: ', CACHE_NAME, error);
+            function(reason) {
+                console.log('Error - Opened cache (install)', CACHE_NAME, reason);
             }
         )
     );
@@ -22,87 +22,46 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
     event.respondWith(
-        fetch(event.request).then(function(response) {
-            console.log('fetch response', response.status, response);
-            if(response && response.status <= 400) {
-                // console.log('cache');
-                var responseToCache = response.clone();
+        fetch(event.request).then(
+            function(response) {
+                console.log('fetch response', response.status, response);
+                if(response && response.status <= 400) {
+                    var responseToCache = response.clone();
 
-                caches.open(CACHE_NAME).then(function(cache) {
-                    console.log("Add to Cache");//, response.status, responseToCache);
+                    caches.open(CACHE_NAME).then(
+                        function(cache) {
+                            console.log("Add to Cache", event.request, responseToCache);
 
-                    cache.put(event.request, responseToCache);
-                });
-                return response;
-            }
-            else { 
-                // error: get from cache
-                caches.open(CACHE_NAME).then(function(cache) {
+                            cache.put(event.request, responseToCache);
+                        },
+                        function(reason) {
+                            console.log("Error - Add to Cache", reason);
+                        });
+                    return response;
+                }
+                else { 
+                    // error: get from cache
+                    caches.open(CACHE_NAME).then(function(cache) {
 
-                    caches.match(event.request).then(function(response) {
-                        // Cache hit - return response
-                        if (response) {
-                          console.log("From Cache: ", response);
-                          return response;
-                        }
-                    }, function(reason) {
-                        return null;
+                        caches.match(event.request).then(
+                            function(response) {
+                                // Cache hit - return response
+                                if (response) {
+                                  console.log("Get from Cache", event.request, response);
+                                  return response;
+                                }
+                            }, function(reason) {
+                                console.log('Error - Get from Cache', reason);
+                                return null;
+                            }
+                        );
                     });
-                });
+                }
+            }, 
+            function(reason) {
+                console.log('Error - Featch', reason);
             }
-        }, function(reason) {
-            console.log('Featch error with reason ', reason);
-        })
+        )
     );
 });
 
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request)
-//       .then(function(response) {
-//         // Cache hit - return response
-//         if (response) {
-//           console.log("From Cache: ", response);
-//           return response;
-//         }
-
-//         // IMPORTANT: Clone the request. A request is a stream and
-//         // can only be consumed once. Since we are consuming this
-//         // once by cache and once by the browser for fetch, we need
-//         // to clone the response.
-//         var fetchRequest = event.request.clone();
-
-//         return fetch(fetchRequest).then(function(response) {
-//             // Check if we received a valid response
-//             if(
-//               response && 
-//               response.status === 200 && 
-//               response.type === 'basic') {
-
-//               // IMPORTANT: Clone the response. A response is a stream
-//               // and because we want the browser to consume the response
-//               // as well as the cache consuming the response, we need
-//               // to clone it so we have two streams.
-//               var responseToCache = response.clone();
-
-//               caches.open(CACHE_NAME)
-//                 .then(function(cache) {
-//                   console.log("Add to Cache: ", response);
-
-//                   cache.put(event.request, responseToCache);
-//                 });
-//             }
-
-//             // else {
-
-//             //   if(response.type !== "opaque")
-//             //     console.log("invalid response: ", response);
-//             // }
-
-//             return response;
-//           }
-//         );
-//       }
-//     )
-//   );
-// });
