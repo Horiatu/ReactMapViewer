96cf85a0a758374331562e289f4f8e7b23299332
Horia Tudosie <htudosie@esri.ca>
2018-02-23 12:28:49 -0500

    service worker from google example
    https://github.com/GoogleChrome/samples/blob/gh-pages/service-worker/prefetch/service-worker.js

--- ReactMapViewer.sw.js
+++ ReactMapViewer.sw.js
@@ -1,75 +1,134 @@
-var CACHE_NAME = 'ReactMapViewer-cache-v1';
-var urlsToCache = [
-  './',
-  './css/styles.css',
-  './offline.html'
-];
+// https://github.com/GoogleChrome/samples/blob/gh-pages/service-worker/prefetch/service-worker.js
+
+const CACHE_VERSION = 1;
+const CACHES = {
+  prefetch: 'ReactMapViewer-cache-v' + CACHE_VERSION
+};
+
 
 self.addEventListener('install', function(event) {
+    const now = Date.now();
+
+    const urlsToCache = [
+        './',
+        './index.html',      
+        './css/styles.css',
+        './lib/jquery-1.12.4.min.js',
+        './lib/jquery-ui.min.js',
+        '//unpkg.com/react@16/umd/react.development.js',
+        '//unpkg.com/react-dom@16/umd/react-dom.development.js',
+        '//unpkg.com/babel-standalone@6.15.0/babel.min.js',
+        '//code.responsivevoice.org/responsivevoice.js',
+        './lib/arcgis.dojo-4.6.js',
+        './js/RMap.js',
+        './js/NavMenu.js',
+        './js/NavBar.js',
+        './js/Panels.js',
+        './js/PanelLegend.js',
+        './js/PanelLayers.js',
+        './js/PanelAdvancedMenu.js',
+        './js/Help.js',
+        './js/Layout.js',
+    ];
+
     event.waitUntil(
-        caches.open(CACHE_NAME).then(
-            (cache) => {
-                // caches.delete(CACHE_NAME).then((obj) => {
-                //     console.log('Deleted cache (install)', CACHE_NAME, obj, cache.keys());
-                    cache.addAll(urlsToCache).then(() => {
-                    console.log('Opened cache (install)', CACHE_NAME, cache.keys());
-                    return cache;
+        caches.open(CACHES.prefetch).then(function(cache) {
+            const cachePromises = urlsToCache.map(function(urlToPrefetch) {
+            // This constructs a new URL object using the service worker's script location as the base
+            // for relative URLs.
+            const url = new URL(urlToPrefetch, location.href);
+            // console.log('URL', url);
+            // Append a cache-bust=TIMESTAMP URL parameter to each URL's query string.
+            // This is particularly important when precaching resources that are later used in the
+            // fetch handler as responses directly, without consulting the network (i.e. cache-first).
+            // If we were to get back a response from the HTTP browser cache for this precaching request
+            // then that stale response would be used indefinitely, or at least until the next time
+            // the service worker script changes triggering the install flow.
+            url.search += (url.search ? '&' : '?') + 'cache-bust=' + now;
+            // It's very important to use {mode: 'no-cors'} if there is any chance that
+            // the resources being fetched are served off of a server that doesn't support
+            // CORS (http://en.wikipedia.org/wiki/Cross-origin_resource_sharing).
+            // In this example, www.chromium.org doesn't support CORS, and the fetch()
+            // would fail if the default mode of 'cors' was used for the fetch() request.
+            // The drawback of hardcoding {mode: 'no-cors'} is that the response from all
+            // cross-origin hosts will always be opaque
+            // (https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#cross-origin-resources)
+            // and it is not possible to determine whether an opaque response represents a success or failure
+            // (https://github.com/whatwg/fetch/issues/14).
+            const request = new Request(url, {mode: 'no-cors'});
+            return fetch(request).then(function(response) {
+                if (response.status >= 400) {
+                    throw new Error('request for ' + urlToPrefetch +
+                      ' failed with status ' + response.statusText);
+                }
+
+                // Use the original URL without the cache-busting parameter as the key for cache.put().
+                return cache.put(urlToPrefetch, response);
+                }).catch(function(error) {
+                    console.error('Not caching:',urlToPrefetch, url, error);
                 });
-            },
-            function(reason) {
-                console.log('Error - Opened cache (install)', CACHE_NAME, reason);
-            }
-        )
+            });
+
+            return Promise.all(cachePromises).then(function() {
+                console.log('Pre-fetching complete.');
+            });
+        }).catch(function(error) {
+            console.error('Pre-fetching failed:', error);
+        })
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    // Delete all caches that aren't named in CURRENT_CACHES.
+    // While there is only one cache in this example, the same logic will handle the case where
+    // there are multiple versioned caches.
+    const expectedCacheNames = Object.keys(CACHES).map(function(key) {
+        return CACHES[key];
+    });
+
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (expectedCacheNames.indexOf(cacheName) === -1) {
+                        // If this cache name isn't present in the array of "expected" cache names, then delete it.
+                        console.log('Deleting out of date cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
 
 self.addEventListener('fetch', function(event) {
+    // console.log('Handling fetch event for', event.request.url);
+
     event.respondWith(
-        fetch(event.request).then(
-            function(response) {
-                // console.log('fetch response', response.status, response);
-                if(response && response.status <= 400) {
-                    var responseToCache = response.clone();
+        // caches.match() will look for a cache entry in all of the caches available to the service worker.
+        // It's an alternative to first opening a specific named cache and then matching on that.
+        caches.match(event.request).then(function(response) {
+        if (response) {
+            // console.log('Found response in cache:', response);
 
-                    caches.open(CACHE_NAME).then(
-                        (cache) => cache.put(event.request, responseToCache),
-                        (reason) => console.log("Error - Add to Cache", reason)
-                    );
-                    return response;
-                }
-                else 
-                { 
-                    // error: get from cache
-                    getFromCache(event.request);
-                }
-            }, 
-            function(reason) {
-                console.log('Error - Featch:', reason);
-                getFromCache(event.request);
-            }
-        )
-    );
-});
-
-function getFromCache(request) {
-    caches.open(CACHE_NAME).then((cache) => {
-        try {
-            caches.match(request).then(
-                (response) => {
-                    // Cache hit - return response
-                    if (response) {
-                          console.log("Get from Cache:", request);
-                          return response;
-                    }
-                }, 
-                (reason) => {
-                    console.log('Error - Get from Cache:', reason);
-                    return cache.match('offline.html');
-                }
-            );
-        } catch (ex) {
-            console.log('Error - Get from Cache (1):', ex.message);
-            return cache.match('offline.html');
+            return response;
         }
-    });    
-};
+
+        console.log('No response found in cache. About to fetch from network...');
+
+        // event.request will always have the proper mode set ('cors, 'no-cors', etc.) so we don't
+        // have to hardcode 'no-cors' like we do when fetch()ing in the install handler.
+        return fetch(event.request).then(function(response) {
+            console.log('Response from network is:', response);
+
+            return response;
+        }).catch(function(error) {
+            // This catch() will handle exceptions thrown from the fetch() operation.
+            // Note that a HTTP error response (e.g. 404) will NOT trigger an exception.
+            // It will return a normal response object that has the appropriate error code set.
+            console.error('Fetching failed:', error);
+
+            throw error;
+        });
+    }));
+});
