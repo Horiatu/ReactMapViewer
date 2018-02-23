fd896208e204ff6ea255e6f13c78ef17fe40526f
Horia Tudosie <htudosie@esri.ca>
2018-02-23 15:12:45 -0500

    removed logging, comments...

--- ReactMapViewer.sw.js
+++ ReactMapViewer.sw.js
@@ -45,27 +45,8 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHES.prefetch).then((cache) => {
             const cachePromises = urlsToCache.map((urlToPrefetch) => {
-            // This constructs a new URL object using the service worker's script location as the base
-            // for relative URLs.
             const url = new URL(urlToPrefetch, location.href);
-            // console.log('URL', url);
-            // Append a cache-bust=TIMESTAMP URL parameter to each URL's query string.
-            // This is particularly important when precaching resources that are later used in the
-            // fetch handler as responses directly, without consulting the network (i.e. cache-first).
-            // If we were to get back a response from the HTTP browser cache for this precaching request
-            // then that stale response would be used indefinitely, or at least until the next time
-            // the service worker script changes triggering the install flow.
             url.search += (url.search ? '&' : '?') + 'cache-bust=' + now;
-            // It's very important to use {mode: 'no-cors'} if there is any chance that
-            // the resources being fetched are served off of a server that doesn't support
-            // CORS (http://en.wikipedia.org/wiki/Cross-origin_resource_sharing).
-            // In this example, www.chromium.org doesn't support CORS, and the fetch()
-            // would fail if the default mode of 'cors' was used for the fetch() request.
-            // The drawback of hardcoding {mode: 'no-cors'} is that the response from all
-            // cross-origin hosts will always be opaque
-            // (https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#cross-origin-resources)
-            // and it is not possible to determine whether an opaque response represents a success or failure
-            // (https://github.com/whatwg/fetch/issues/14).
             const request = new Request(url, {mode: 'no-cors'});
             return fetch(request).then((response) => {
                 if (response.status >= 400) {
@@ -90,9 +71,6 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
-    // Delete all caches that aren't named in CURRENT_CACHES.
-    // While there is only one cache in this example, the same logic will handle the case where
-    // there are multiple versioned caches.
     const expectedCacheNames = Object.keys(CACHES).map((key) => {
         return CACHES[key];
     });
@@ -113,36 +91,23 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-    // console.log('Handling fetch event for', event.request.url);
-
     event.respondWith(
-        // caches.match() will look for a cache entry in all of the caches available to the service worker.
-        // It's an alternative to first opening a specific named cache and then matching on that.
         caches.match(event.request).then((response) => {
         if (response) {
             console.log('Found response in cache:', response);
 
             return response;
         }
-
-        // console.log('No response found in cache. About to fetch from network...');
-
-        // event.request will always have the proper mode set ('cors, 'no-cors', etc.) so we don't
-        // have to hardcode 'no-cors' like we do when fetch()ing in the install handler.
         return fetch(event.request).then((response) => {
             if(response.url !== '' || response.type !== 'opaque')
                 console.log('Response from network is:', response);
 
             return response;
         }).catch((error) => {
-            // This catch() will handle exceptions thrown from the fetch() operation.
-            // Note that a HTTP error response (e.g. 404) will NOT trigger an exception.
-            // It will return a normal response object that has the appropriate error code set.
             console.error('Fetching failed:', error);
             caches.open(CACHES.prefetch).then((cache) => {
                 return cache.match('offline.html');
             });
-
             // throw error;
         });
     }));
