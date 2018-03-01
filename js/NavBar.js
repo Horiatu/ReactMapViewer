b4ffd2a2ac970b9b3deac8f420fe3d9e7596e166
Horia Tudosie <htudosie@esri.ca>
2018-03-01 12:17:09 -0500

    4 sized tabs

--- js/NavBar.js
+++ js/NavBar.js
@@ -1,52 +1,52 @@
 class NavBar extends React.Component {
-    render() {
-        // console.log('NAVBAR', this);
-        return (
-            <nav className="navbar calcite-navbar navbar-fixed-top calcite-text-light calcite-bg-dark">
-                <NavMenu />
-                <div className="calcite-title calcite-overflow-hidden">
-                    <span className="calcite-title-main">React Map Viewer</span>
-                    <span className="calcite-title-divider hidden-xs" />
-                    <span className="calcite-title-sub hidden-xs">
-                        Responsive Design: A Bootstrap theme for building modern
-                        map apps
-                    </span>
-                </div>
-                <ul className="nav navbar-nav calcite-nav">
-                    <li>
-                        <div className="calcite-navbar-search calcite-search-expander">
-                            <div id="searchWidgetDiv" />
-                        </div>
-                    </li>
-                </ul>
-            </nav>
-        );
-    }
-    componentDidMount() {
-        const self = this;
-        require([
-            "esri/widgets/Search",
-            "calcite-maps/calcitemaps-arcgis-support-v0.5",
-            "dojo/domReady!"
-        ], function(Search, CalciteMapArcGISSupport) {
-            const searchWidget = new Search({
-                container: "searchWidgetDiv",
-                view: self.props.mapView,
-                locationEnabled: true
-            });
-            CalciteMapArcGISSupport.setSearchExpandEvents(searchWidget);
-            searchWidget.on("search-focus", function(ev) {
-                // console.log('search focus', ev, searchWidget);
-                responsiveVoice.speak(ev.target.allPlaceholder);
-            });
-            searchWidget.on("search-start", function(ev) {
-                // console.log("Search started", ev);
-                responsiveVoice.speak("Search");
-            });
-            searchWidget.on("search-complete", function(ev) {
-                // console.log('Search complete', ev, searchWidget);
-                responsiveVoice.speak(ev.results[0].results[0].name);
-            });
-        });
-    }
+	render() {
+		// console.log('NAVBAR', this);
+		return (
+			<nav className="navbar calcite-navbar navbar-fixed-top calcite-text-light calcite-bg-dark">
+				<NavMenu />
+				<div className="calcite-title calcite-overflow-hidden">
+					<span className="calcite-title-main">React Map Viewer</span>
+					<span className="calcite-title-divider hidden-xs" />
+					<span className="calcite-title-sub hidden-xs">
+						Responsive Design: A Bootstrap theme for building modern
+						map apps
+					</span>
+				</div>
+				<ul className="nav navbar-nav calcite-nav">
+					<li>
+						<div className="calcite-navbar-search calcite-search-expander">
+							<div id="searchWidgetDiv" />
+						</div>
+					</li>
+				</ul>
+			</nav>
+		);
+	}
+	componentDidMount() {
+		const self = this;
+		require([
+			"esri/widgets/Search",
+			"calcite-maps/calcitemaps-arcgis-support-v0.5",
+			"dojo/domReady!"
+		], function(Search, CalciteMapArcGISSupport) {
+			const searchWidget = new Search({
+				container: "searchWidgetDiv",
+				view: self.props.mapView,
+				locationEnabled: true
+			});
+			CalciteMapArcGISSupport.setSearchExpandEvents(searchWidget);
+			searchWidget.on("search-focus", function(ev) {
+				// console.log('search focus', ev, searchWidget);
+				responsiveVoice.speak(ev.target.allPlaceholder);
+			});
+			searchWidget.on("search-start", function(ev) {
+				// console.log("Search started", ev);
+				responsiveVoice.speak("Search");
+			});
+			searchWidget.on("search-complete", function(ev) {
+				// console.log('Search complete', ev, searchWidget);
+				responsiveVoice.speak(ev.results[0].results[0].name);
+			});
+		});
+	}
 }
--- js/PanelLayers.js
+++ js/PanelLayers.js
@@ -1,49 +1,49 @@
 class PanelLayers extends Panel {
-  render() {
-    return super.renderContent(
-      "Layers",
-      function() {
-        return <div id="layersDiv" />;
-      },
-      "glyphicon-list"
-    );
-  }
-  //
-  constructor(props) {
-    super(props);
-    const self = this;
-    require([
-      "esri/widgets/LayerList",
-      "dojo/query",
-      "dojo/dom-attr",
-      "dojo/domReady!"
-    ], (LayerList, query, domAttr) => {
-      const layerList = new LayerList({
-        container: "layersDiv",
-        view: self.props.mapView
-      });
+    render() {
+        return super.renderContent(
+            "Layers",
+            function() {
+                return <div id="layersDiv" />;
+            },
+            "glyphicon-list"
+        );
+    }
+    //
+    constructor(props) {
+        super(props);
+        const self = this;
+        require([
+            "esri/widgets/LayerList",
+            "dojo/query",
+            "dojo/dom-attr",
+            "dojo/domReady!"
+        ], (LayerList, query, domAttr) => {
+            const layerList = new LayerList({
+                container: "layersDiv",
+                view: self.props.mapView
+            });
 
-      const layersNode = dojo.byId("layersDiv");
-      if (layersNode) {
-        new MutationObserver(mutations => {
-          // console.log('mutations', mutations);
-          const labels = query(".esri-layer-list__item-title");
-          // console.log('labels', labels);
+            const layersNode = dojo.byId("layersDiv");
+            if (layersNode) {
+                new MutationObserver(mutations => {
+                    // console.log('mutations', mutations);
+                    const labels = query(".esri-layer-list__item-title");
+                    // console.log('labels', labels);
 
-          labels.forEach(label => {
-            domAttr.set(
-              label,
-              "onfocus",
-              'responsiveVoice.speak("' + label.innerText + '")'
-            );
-            // console.log(label)
-          });
-        }).observe(layersNode, {
-          attributes: false,
-          childList: true,
-          characterData: false
+                    labels.forEach(label => {
+                        domAttr.set(
+                            label,
+                            "onfocus",
+                            'responsiveVoice.speak("' + label.innerText + '")'
+                        );
+                        // console.log(label)
+                    });
+                }).observe(layersNode, {
+                    attributes: false,
+                    childList: true,
+                    characterData: false
+                });
+            }
         });
-      }
-    });
-  }
+    }
 }
