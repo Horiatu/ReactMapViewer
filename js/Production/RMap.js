316c29466bc763f370fcee84611b4f91e2ca6a38
Horia Tudosie <Horiatu@users.noreply.github.com>
2018-04-30 15:24:41 -0400

    config tools helper functions

--- js/Production/RMap.js
+++ js/Production/RMap.js
@@ -7,10 +7,6 @@ class RMap extends React.Component {
                 "div",
                 { className: "calcite-map calcite-map-absolute" },
                 React.createElement("div", { id: "mapViewDiv" }),
-                // React.createElement(AppConfig, {
-                //     appId: "b54efa235b7f455f91b14396090ad3e3",
-                //     portalUrl: "http://esrica-tsg.maps.arcgis.com"
-                // }, null),
                 React.createElement(
                     "div",
                     {
@@ -49,6 +45,10 @@ class RMap extends React.Component {
             mapView: null
         };
         const self = this;
+
+        // const hasTool = function(this, name) {
+
+        // };
 
         require([
             "dojo/dom-class",
@@ -98,49 +98,57 @@ class RMap extends React.Component {
                 // console.log("appConfig item",r);
                 r.fetchData().then(d => {
                     self.config = d.values;
-                    console.log("appConfig", self.config);
+                    self.config.hasTool = function(name) {
+                        return this.hasOwnProperty('tool_'+name) && this['tool_'+name];
+                    };
+                    self.config.allTools = function() {
+                        return Object.keys(this).filter(tool => tool.indexOf('tool_')===0 && this[tool]).map(tool => tool.slice(5));
+                    };
+                    // console.log("allTools", self.config.allTools());
+                    // console.log("appConfig", self.config);
 
-                // Map
-                const map = new WebMap({
-                    portalItem: {
-                        id: self.config.webmap 
-                    }
+                    // Map
+                    const map = new WebMap({
+                        portalItem: {
+                            id: self.config.webmap 
+                        }
+                    });
+
+                    // View
+                    const mapView = new MapView({
+                        container: "mapViewDiv",
+                        map: map,
+                        padding: {
+                            top: 50,
+                            bottom: 0
+                        }
+                    });
+
+                    self.setState({
+                        map: map,
+                        mapView: mapView
+                    });
+
+                    // Popup and panel sync
+                    mapView.then(function() {
+                        CalciteMapArcGISSupport.setPopupPanelSync(mapView);
+                        domClass.remove(document.body, "app-loading");
+                    });
+
+                    // BasemapToggle *
+                    var basemapToggle = new BasemapToggle({
+                        view: mapView,
+                        secondBasemap: "satellite"
+                    });
+                    mapView.ui.add(basemapToggle, "bottom-right");
+
+                    // // Scalebar
+                    // var scaleBar = new ScaleBar({
+                    //     view: mapView
+                    // });
+                    // mapView.ui.add(scaleBar, "bottom-left");
+                                    
                 });
-
-                // View
-                const mapView = new MapView({
-                    container: "mapViewDiv",
-                    map: map,
-                    padding: {
-                        top: 50,
-                        bottom: 0
-                    }
-                });
-
-                self.setState({
-                    map: map,
-                    mapView: mapView
-                });
-
-                // Popup and panel sync
-                mapView.then(function() {
-                    CalciteMapArcGISSupport.setPopupPanelSync(mapView);
-                    domClass.remove(document.body, "app-loading");
-                });
-
-                // BasemapToggle *
-                var basemapToggle = new BasemapToggle({
-                    view: mapView,
-                    secondBasemap: "satellite"
-                });
-                mapView.ui.add(basemapToggle, "bottom-right");
-
-                // // Scalebar
-                // var scaleBar = new ScaleBar({
-                //     view: mapView
-                // });
-                // mapView.ui.add(scaleBar, "bottom-left");
-                                });
             }, 
             error => console.log('error', error));
         });
