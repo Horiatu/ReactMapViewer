d160db324fb916689ddf4a1775ecaf588fc6f2f4
Horia Tudosie <Horiatu@users.noreply.github.com>
2018-05-08 11:44:05 -0400

    No Layout, no Production folder

--- js/RMap.js
+++ js/RMap.js
@@ -1,4 +1,26 @@
-class RMap extends React.Component {
+class ErrorBoundary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { hasError: false };
+    }
+
+    componentDidCatch(error, info) {
+        // Display fallback UI
+        this.setState({ hasError: true });
+        // You can also log the error to an error reporting service
+        console.log("Error - ErrorBoundary", error, info);
+    }
+
+    render() {
+        if (this.state.hasError) {
+            // You can render any custom fallback UI
+            return <h1>Something went wrong.</h1>;
+        }
+        return this.props.children;
+    }
+}
+
+class RMap extends ErrorBoundary {
     render() {
         return (
             <div>
@@ -6,11 +28,11 @@ class RMap extends React.Component {
                     <div id="mapViewDiv" />
 
                     <div className="calcite-panels calcite-panels-right calcite-text-light calcite-bg-dark panel-group">
-                        <PanelAbout />
-                        <PanelBasemaps mapView={this.state.mapView} />
-                        <PanelLegend mapView={this.state.mapView} />
-                        <PanelLayers mapView={this.state.mapView} />
-                        <PanelPrint mapView={this.state.mapView} />
+                        <PanelAbout/>
+                        <PanelBasemaps/>
+                        <PanelLegend/>
+                        <PanelLayers/>
+                        <PanelPrint/>
                         <PanelAdvancedMenu />
                     </div>
                 </div>
@@ -22,89 +44,250 @@ class RMap extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            map: null,
-            mapView: null
+            config: null
         };
-        const self = this;
+        // const self = this;
 
-        require([
-            "dojo/dom-class",
-            // ArcGIS
-            "esri/WebMap",
-            "esri/views/MapView",
+        new AppConfig({
+            appId: this.props.appId,
+            portalUrl: this.props.portalUrl,
+            voice: false
+        }).then(config => {
 
-            // Widgets
-            "esri/widgets/BasemapToggle",
-            "esri/widgets/ScaleBar",
+                this.setState({
+                    config: config,
+                });
 
-            // Bootstrap
-            "bootstrap/Collapse",
-            "bootstrap/Dropdown",
+                console.log('config', config);
+                require([
+                    "dojo/dom-class",
+                    // ArcGIS
+                    "esri/WebMap",
+                    "esri/views/MapView",
 
-            // Calcite Maps
-            "calcite-maps/calcitemaps-v0.5",
-            // Calcite Maps ArcGIS Support
-            "calcite-maps/calcitemaps-arcgis-support-v0.5",
-            "dojo/domReady!"
-        ], function(
-            domClass,
-            WebMap,
-            MapView,
-            Print,
-            BasemapToggle,
-            ScaleBar,
-            Collapse,
-            Dropdown,
-            CalciteMaps,
-            CalciteMapArcGISSupport
-        ) {
-            /******************************************************************
-             *
-             * Create the map, view and widgets
-             *
-             ******************************************************************/
-            // console.log('RMAP', self);
+                    // Bootstrap
+                    "bootstrap/Collapse",
+                    "bootstrap/Dropdown",
 
-            // Map
-            const map = new WebMap({
-                portalItem: {
-                    id: self.props.itemId
-                }
-            });
+                    // Calcite Maps
+                    "calcite-maps/calcitemaps-v0.5",
+                    // Calcite Maps ArcGIS Support
+                    "calcite-maps/calcitemaps-arcgis-support-v0.5",
+                    // "dojo/domReady!"
+                ], function(
+                    domClass,
+                    WebMap,
+                    MapView,
+                    Collapse,
+                    Dropdown,
+                    CalciteMaps,
+                    CalciteMapArcGISSupport) {
+                    // console.log('promise response', config);
 
-            // View
-            const mapView = new MapView({
-                container: "mapViewDiv",
-                map: map,
-                padding: {
-                    top: 50,
-                    bottom: 0
-                }
-            });
+                    // const map = new WebMap({
+                    //     portalItem: {
+                    //         id: config.webmap 
+                    //     }
+                    // });
 
-            self.setState({
-                map: map,
-                mapView: mapView
-            });
+                    MapView({
+                        container: "mapContainer",
+                        map: new WebMap({
+                            portalItem: {
+                                id: config.webmap
+                            }
+                        }),
+                        padding: {
+                            top: 50,
+                            // bottom: 0
+                        }
+                    }).when(function(mapView) {
 
-            // Popup and panel sync
-            mapView.when(function() {
-                CalciteMapArcGISSupport.setPopupPanelSync(mapView);
-                domClass.remove(document.body, "app-loading");
-            });
+                        CalciteMapArcGISSupport.setPopupPanelSync(mapView);
+                        domClass.remove(document.body, "app-loading");
 
-            // BasemapToggle *
-            var basemapToggle = new BasemapToggle({
-                view: mapView,
-                secondBasemap: "satellite"
-            });
-            mapView.ui.add(basemapToggle, "bottom-right");
+                        if (AppConfig.hasTool(config, 'basemap')) {
+                            require([
+                                    "esri/widgets/BasemapGallery",
+                                    "dojo/domReady!"
+                                ],
+                                BasemapGallery => new BasemapGallery({
+                                    container: "basemapGalleryDiv",
+                                    view: mapView
+                                }));
+                        }
 
-            // Scalebar
-            var scaleBar = new ScaleBar({
-                view: mapView
-            });
-            mapView.ui.add(scaleBar, "bottom-left");
-        });
+                        if (AppConfig.hasTool(config, 'legend')) {
+                            require([
+                                    "esri/widgets/Legend",
+                                    "dojo/query",
+                                    "dojo/dom-attr",
+                                    "dojo/dom-style",
+                                    "dojo/domReady!"
+                                ],
+                                (Legend, query, domAttr, domStyle) => {
+                                    const legend = new Legend({
+                                        container: "legendDiv",
+                                        view: mapView
+                                    });
+
+                                    const legendNode = dojo.byId("legendDiv");
+                                    if (legendNode) {
+                                        new MutationObserver(function(mutations) {
+                                            mutations.forEach(function(mutation) {
+                                                if (
+                                                    mutation.addedNodes &&
+                                                    mutation.addedNodes.length >= 1
+                                                ) {
+                                                    for (
+                                                        var i = 0; i < mutation.addedNodes.length; i++
+                                                    ) {
+                                                        var node = mutation.addedNodes[i];
+                                                        if (domStyle.get(node, "display") !== "none") {
+                                                            const serviceLabels = query(
+                                                                ".esri-legend__service-label, .esri-legend__layer-caption, .esri-legend__layer-row"
+                                                            );
+                                                            // console.log(node, serviceLabels);
+                                                            serviceLabels.forEach(label => {
+                                                                domAttr.set(label, "tabindex", 0);
+                                                                domAttr.set(
+                                                                    label,
+                                                                    "onfocus",
+                                                                    'responsiveVoice.speak("' +
+                                                                    label.innerText +
+                                                                    '")'
+                                                                );
+                                                                // console.log(label)
+                                                            });
+                                                        }
+                                                    }
+                                                }
+                                            });
+                                        }).observe(legendNode, {
+                                            attributes: true,
+                                            childList: true,
+                                            characterData: false
+                                        });
+                                    }
+                                });
+                        }
+
+                        if (AppConfig.hasTool(config, 'layers')) {
+                            require([
+                                    "esri/widgets/LayerList",
+                                    "dojo/query",
+                                    "dojo/dom-attr",
+                                    "dojo/domReady!"
+                                ],
+                                (LayerList, query, domAttr) => {
+                                    const layerList = new LayerList({
+                                        container: "layersDiv",
+                                        view: mapView
+                                    });
+
+                                    const layersNode = dojo.byId("layersDiv");
+                                    if (layersNode) {
+                                        new MutationObserver(mutations => {
+                                            // console.log('mutations', mutations);
+                                            const labels = query(".esri-layer-list__item-title");
+                                            // console.log('labels', labels);
+
+                                            labels.forEach(label => {
+                                                domAttr.set(
+                                                    label,
+                                                    "onfocus",
+                                                    'responsiveVoice.speak("' + label.innerText + '")'
+                                                );
+                                                // console.log(label)
+                                            });
+                                        }).observe(layersNode, {
+                                            attributes: false,
+                                            childList: true,
+                                            characterData: false
+                                        });
+                                    }
+                                });
+                        }
+
+                        if (AppConfig.hasTool(config, 'print')) {
+                            require([
+                                    "esri/widgets/Print",
+                                    "dojo/domReady!"
+                                ],
+                                Print => new Print({
+                                    container: "printDiv",
+                                    view: mapView,
+                                    printServiceUrl: "https://utility.arcgisonline.com/arcgis/rest/services/Utilities/PrintingTools/GPServer/Export%20Web%20Map%20Task"
+                                }));
+                        }
+
+                        if (AppConfig.hasTool(config, 'search')) {
+                            require([
+                                "esri/widgets/Search",
+                                "calcite-maps/calcitemaps-arcgis-support-v0.5",
+                                "dojo/domReady!"
+                            ], function(Search, CalciteMapArcGISSupport) {
+                                const searchWidget = new Search({
+                                    container: "searchWidgetDiv",
+                                    view: mapView,
+                                    locationEnabled: true
+                                });
+                                CalciteMapArcGISSupport.setSearchExpandEvents(searchWidget);
+                                searchWidget.on("search-focus", function(ev) {
+                                    // console.log('search focus', ev, searchWidget);
+                                    responsiveVoice.speak(ev.target.allPlaceholder);
+                                });
+                                searchWidget.on("search-start", function(ev) {
+                                    // console.log("Search started", ev);
+                                    responsiveVoice.speak("Search");
+                                });
+                                searchWidget.on("search-complete", function(ev) {
+                                    // console.log('Search complete', ev, searchWidget);
+                                    responsiveVoice.speak(ev.results[0].results[0].name);
+                                });
+                            });
+                        }
+
+                        require([
+                                "esri/widgets/BasemapToggle",
+                                "dojo/domReady!"
+                            ],
+                            BasemapToggle => {
+                                const basemapToggle = new BasemapToggle({
+                                    view: mapView,
+                                    secondBasemap: "satellite"
+                                });
+                                mapView.ui.add(basemapToggle, "bottom-right");
+                            });
+
+                        if (config.scalebar) {
+                            require([
+                                    "esri/widgets/ScaleBar",
+                                    "dojo/domReady!"
+                                ],
+                                ScaleBar => {
+                                    mapView.ui.add(new ScaleBar({
+                                        view: mapView
+                                    }), "bottom-left");
+                                });
+                        }
+                    });
+
+                    if (config.voice && responsiveVoice && responsiveVoice.voiceSupport()) {
+                        responsiveVoice.speak("React Map Viewer");
+                        $(".calcite-title-main").click(ev => {
+                            responsiveVoice.speak(ev.currentTarget.innerText);
+                        });
+                    } else {
+                        responsiveVoice = {
+                            speak: function(text) { /*console.log('"'+text+'" (muted)');*/ }
+                        };
+                    }
+                });
+            },
+            error => {
+                console.log('Error reading configuration', error);
+            }
+        )
     }
 }
